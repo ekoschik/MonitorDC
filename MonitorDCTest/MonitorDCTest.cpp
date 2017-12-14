0c1aaf48a55cc01ceab3259c73d57e8a1f8a20ad
Evan Koschik <ekoschik@umich.edu>
2017-12-13 23:13:48 -0800

    remove double click since using button down

--- MonitorDCTest/MonitorDCTest.cpp
+++ MonitorDCTest/MonitorDCTest.cpp
@@ -93,7 +93,7 @@ int main()
 
     WNDCLASSEXW wcex = {};
     wcex.cbSize = sizeof(WNDCLASSEX);
-    wcex.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
+    wcex.style = CS_HREDRAW | CS_VREDRAW /*| CS_DBLCLKS*/;
     wcex.lpfnWndProc = WndProc;
     wcex.cbClsExtra = 0;
     wcex.cbWndExtra = 0;
